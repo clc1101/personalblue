bb3317cea44b04359f3e1b2488d7eec7ee778a60
clc1101 <151733110+clc1101@users.noreply.github.com>
2024-07-23 08:17:25 +0200

    Delete gnome-extensions.sh

--- gnome-extensions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Blur my shell
-wget -P /tmp https://extensions.gnome.org/extension-data/blur-my-shellaunetx.v62.shell-extension.zip
-unzip -d /tmp/tmpdir /tmp/blur-my-shellaunetx.v62.shell-extension.zip
-UUID=$(jq -r '.["uuid"]' < '/tmp/tmpdir/metadata.json')
-cp tmp/tmpdir /usr/share/gnome-shell/extensions/${UUID}
-rm /tmp/tmpdir
